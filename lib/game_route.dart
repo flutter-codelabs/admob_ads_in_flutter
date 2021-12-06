f0e0a641d508c9544798c531f85c9617ad79ffb9
nbxtruong <omega220@gmail.com>
2021-12-06 15:12:05 +0700

    (+) Add a banner ad

---
+++ lib/game_route.dart
@@ -0,0 +1,300 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import 'app_theme.dart';
+import 'drawing.dart';
+import 'drawing_painter.dart';
+import 'quiz_manager.dart';
+import 'ad_helper.dart';
+
+class GameRoute extends StatefulWidget {
+  @override
+  _GameRouteState createState() => _GameRouteState();
+}
+
+class _GameRouteState extends State<GameRoute> implements QuizEventListener {
+  late int _level;
+
+  late Drawing _drawing;
+
+  late String _clue;
+
+  // ignore: todo
+  // TODO: Add _bannerAd
+  late BannerAd _bannerAd;
+
+  // ignore: todo
+  // TODO: Add _isBannerAdReady
+  bool _isBannerAdReady = false;
+
+  // ignore: todo
+  // TODO: Add _interstitialAd
+
+  // ignore: todo
+  // TODO: Add _isInterstitialAdReady
+
+  // ignore: todo
+  // TODO: Add _rewardedAd
+
+  // ignore: todo
+  // TODO: Add _isRewardedAdReady
+
+  @override
+  void initState() {
+    super.initState();
+
+    QuizManager.instance
+      ..listener = this
+      ..startGame();
+
+    // ignore: todo
+    // TODO: Initialize _bannerAd
+    _bannerAd = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdReady = true;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          // ignore: avoid_print
+          print('Failed to load a banner ad: ${err.message}');
+          _isBannerAdReady = false;
+          ad.dispose();
+        },
+      ),
+    );
+
+    _bannerAd.load();
+
+    // ignore: todo
+    // TODO: Initialize _rewardedAd
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.primary,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Text(
+                    'Level $_level/5',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          String _answer = '';
+
+                          return AlertDialog(
+                            title: Text('Enter your answer'),
+                            content: TextField(
+                              autofocus: true,
+                              onChanged: (value) {
+                                _answer = value;
+                              },
+                            ),
+                            actions: [
+                              TextButton(
+                                child: Text('submit'.toUpperCase()),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                  QuizManager.instance.checkAnswer(_answer);
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24.0,
+                        vertical: 16.0,
+                      ),
+                      child: Text(
+                        _clue,
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Card(
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(24),
+                          child: CustomPaint(
+                            size: Size(300, 300),
+                            painter: DrawingPainter(
+                              drawing: _drawing,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    margin: EdgeInsets.all(16),
+                  ),
+                ],
+              ),
+            ),
+            // ignore: todo
+            // TODO: Display a banner when ready
+            if (_isBannerAdReady)
+              Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  width: _bannerAd.size.width.toDouble(),
+                  height: _bannerAd.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAd),
+                ),
+              ),
+          ],
+        ),
+      ),
+      floatingActionButton: _buildFloatingActionButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BottomAppBar(
+        child: ButtonBar(
+          alignment: MainAxisAlignment.start,
+          children: [
+            TextButton(
+              child: Text('Skip this level'.toUpperCase()),
+              onPressed: () {
+                QuizManager.instance.nextLevel();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget? _buildFloatingActionButton() {
+    // ignore: todo
+    // TODO: Return a FloatingActionButton if a Rewarded Ad is available
+    return null;
+  }
+
+  void _moveToHome() {
+    Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  // ignore: todo
+  // TODO: Implement _loadInterstitialAd()
+
+  // ignore: todo
+  // TODO: Implement _loadRewardedAd()
+
+  @override
+  void dispose() {
+    // ignore: todo
+    // TODO: Dispose a BannerAd object
+    _bannerAd.dispose();
+
+    // ignore: todo
+    // TODO: Dispose an InterstitialAd object
+
+    // ignore: todo
+    // TODO: Dispose a RewardedAd object
+
+    QuizManager.instance.listener = null;
+
+    super.dispose();
+  }
+
+  @override
+  void onWrongAnswer() {
+    _showSnackBar('Wrong answer!');
+  }
+
+  @override
+  void onNewLevel(int level, Drawing drawing, String clue) {
+    setState(() {
+      _level = level;
+      _drawing = drawing;
+      _clue = clue;
+    });
+
+    // ignore: todo
+    // TODO: Load an Interstitial Ad
+  }
+
+  @override
+  void onLevelCleared() {
+    _showSnackBar('Good job!');
+  }
+
+  @override
+  void onGameOver(int correctAnswers) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Game over!'),
+          content: Text('Score: $correctAnswers/5'),
+          actions: [
+            TextButton(
+              child: Text('close'.toUpperCase()),
+              onPressed: () {
+                // ignore: todo
+                // TODO: Display an Interstitial Ad
+
+                _moveToHome();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void onClueUpdated(String clue) {
+    setState(() {
+      _clue = clue;
+    });
+
+    _showSnackBar('You\'ve got one more clue!');
+  }
+}
